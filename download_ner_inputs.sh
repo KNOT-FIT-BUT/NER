e006c2b94d272285e919b9a900d10545b100c7fa
Jan Doležal <idolezal@fit.vutbr.cz>
2020-07-27 16:53:04 +0200

    Made possible to download Ner inputs for other languages than czech

--- download_ner_inputs.sh
+++ download_ner_inputs.sh
@@ -1,27 +1,37 @@
-#!/bin/bash 
+#!/bin/bash
+
+set -e
 
 PWD_COPY=$PWD
 SCRIPT_DIR=$(dirname $0)
 
 echo $SCRIPT_DIR
 
-INPUTS_DIR=$SCRIPT_DIR/ner/inputs
+INPUTS_DIR=$SCRIPT_DIR/ner_lib/inputs
 INPUTS_BACKUP_DIR=$(dirname $INPUTS_DIR)/inputs_backup
 
 TMP_FILE=NER_ML_inputs.tgz
 
-rm -rf $INPUTS_BACKUP_DIR
-mv $INPUTS_DIR $INPUTS_BACKUP_DIR
+[ -d "$INPUTS_BACKUP_DIR" ] && rm -r "$INPUTS_BACKUP_DIR"
+[ -d "$INPUTS_DIR" ] && mv "$INPUTS_DIR" "$INPUTS_BACKUP_DIR"
 
-mkdir $INPUTS_DIR
-cd $INPUTS_DIR
+# <languages>
+downloadNerInputs() {
+    local language="${1?}"
+    local url="${2?}"
+    
+    mkdir -p "${INPUTS_DIR}/${language}"
+    pushd "${INPUTS_DIR}/${language}"
+    wget -c "${url}" -O "${TMP_FILE}"
+    tar -xvf "${TMP_FILE}"
+    rm -f "${TMP_FILE}"
+    popd
+    
+    echo
+}
 
-wget http://knot.fit.vutbr.cz/NAKI_CPK/NER_ML_inputs/NER_ML_inputs-KB_latest-dct.tgz -O $TMP_FILE
-tar -xvf $TMP_FILE 
-
-rm -f $TMP_FILE
-
-cd ..
-mv ner_namedict.pkl ner_namedict.pkl.bak 2>/dev/null
+downloadNerInputs 'cz' 'http://knot.fit.vutbr.cz/NAKI_CPK/NER_ML_inputs/NER_ML_inputs-KB_latest-dct.tgz'
+downloadNerInputs 'en' 'http://athena3.fit.vutbr.cz/kb/latest/NAKI_CPK/ner_inputs.tgz'
+# </languages>
 
 cd $PWD_COPY
