066ce36ee4ffbad51a917cf83d0b9f6435306aec
Jan Doležal <idolezal@fit.vutbr.cz>
2020-09-25 18:45:19 +0200

    NameRecognizer: Fixed printing warning to stdout

--- name_recognizer/item_list.py
+++ name_recognizer/item_list.py
@@ -7,6 +7,7 @@
 
 from .base_list import BaseList
 import os
+import sys
 
 class ItemList(BaseList):
     '''
@@ -36,7 +37,7 @@ class ItemList(BaseList):
             for line in f.readlines():
                 tlist.append(line.strip())
         except IOError:
-            print('File {} not found!'.format(self.path))
+            print('File {} not found!'.format(self.path), file=sys.stderr)
             return []
         else:
             f.close()
