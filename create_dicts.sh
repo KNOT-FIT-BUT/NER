cdc9263284d0ee156cd63e590b9088becead2afe
Volf Tomáš <Vlczech@users.noreply.github.com>
2021-03-11 14:05:11 +0100

    Fix help

--- create_dicts.sh
+++ create_dicts.sh
@@ -13,7 +13,7 @@ DICTS_ARGS=()
 
 usage()
 {
-  echo "Usage: create_dicts.sh --lang=<language> [-u [<login>]] [-c | --clean-cached] [-k <KB path>] [--dev] [--log]"
+  echo "Usage: ./create_dicts.sh --lang=<language> [-u [<login>] [--dev]] [-c | --clean-cached] [-k <KB path>] [--log]"
   echo ""
   echo -e "  -h --help"
   echo -e "  --lang=${DEFAULT_LANG}    Create dictionaries / automatas for given langueage."
